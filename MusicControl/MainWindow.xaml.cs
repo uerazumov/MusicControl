ff715132b2493ce4460c71dd865b7b744cd470ee
Юрий <urist9112@rambler.ru>
2019-08-17 21:47:30 +0300

    Initial commit

---
+++ MusicControl/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MusicControl
+{
+    public partial class MainWindow : Window
+    {
+        private readonly ViewModel _vm;
+        public MainWindow()
+        {
+            _vm = (ViewModel)Application.Current.Resources["ViewModel"];
+            InitializeComponent();
+        }
+
+        private void OnWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            _vm.AssignMainWindow(this);
+            //Main.NavigationService.Navigated += (obj, args) => { Main.NavigationService.RemoveBackEntry(); };
+        }
+    }
+}
---
+++ MusicControl/ViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Navigation;
+
+namespace MusicControl
+{
+    class ViewModel
+    {
+        private MainWindow _mainWindow;
+        private NavigationService _navigationService;
+
+        public ViewModel() { }
+        public void AssignMainWindow(MainWindow mw)
+        {
+            _mainWindow = mw;
+        }
+    }
+}
