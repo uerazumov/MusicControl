b80d2fa4b11b96dce7d8f3ffb733da8a643ab660
Julistian <urist9112@rambler.ru>
2019-08-22 16:30:05 +0300

    Добавлены картинки для кнопки Play

---
+++ MusicControl/ControlButton.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MusicControl
+{
+    public partial class ControlButton : INotifyPropertyChanged
+    {
+        public ControlButton()
+        {
+            InitializeComponent();
+            DataContext = this;
+            IsButtonEnabled = true;
+        }
+
+        public ImageBrush DisableBackgroundImage { get; set; }
+        public ImageBrush DisableBackgroundImageActive { get; set; }
+        public ImageBrush EnableBackgroundImage { get; set; }
+        public ImageBrush EnableBackgroundImageActive { get; set; }
+
+        public ICommand ControlCommand
+        {
+            get { return (ICommand)GetValue(ControlCommandProperty); }
+            set
+            {
+                SetValue(ControlCommandProperty, value);
+                DoPropertyChanged("ControlCommand");
+            }
+        }
+
+        public static DependencyProperty ControlCommandProperty =
+            DependencyProperty.Register("ControlCommand", typeof(ICommand), typeof(ControlButton));
+
+        public static DependencyProperty IsButtonEnabledProperty =
+            DependencyProperty.Register("IsButtonEnabled", typeof(bool), typeof(ControlButton), new UIPropertyMetadata(false, Refresh));
+
+        private bool _isButtonEnabled;
+
+        public bool IsButtonEnabled
+        {
+            get { return _isButtonEnabled; }
+            set
+            {
+                _isButtonEnabled = value;
+                IsEnabled = value;
+                DoPropertyChanged("DisableButton");
+                DoPropertyChanged("BackgroundImageActive");
+                DoPropertyChanged("BackgroundImage");
+            }
+        }
+
+        public static void Refresh(DependencyObject property, DependencyPropertyChangedEventArgs args)
+        {
+            ControlButton circularButton = (ControlButton)property;
+            circularButton.IsButtonEnabled = (bool)args.NewValue;
+            circularButton.DoPropertyChanged("IsButtonEnabled");
+            circularButton.DoPropertyChanged("BackgroundImageActive");
+            circularButton.DoPropertyChanged("BackgroundImage");
+            circularButton.DoPropertyChanged("IsEnabled");
+        }
+
+        public ImageBrush BackgroundImageActive => IsButtonEnabled ? DisableBackgroundImageActive : EnableBackgroundImageActive;
+
+        public ImageBrush BackgroundImage => IsButtonEnabled ? DisableBackgroundImage : EnableBackgroundImage;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void DoPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
--- MusicControl/obj/Debug/MusicControl_Content.g.i.cs
+++ MusicControl/obj/Debug/MusicControl_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("visualresources/icon.ico")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("visualresources/playbuttondisabled.png")]
 
 
