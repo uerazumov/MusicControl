b08b268051109e418781463fe29c2485c90ab582
Julistian <urist9112@rambler.ru>
2019-08-22 16:54:27 +0300

    Добавлены картинки для кнопок Pause, Stop и MainMenu

--- MusicControl/obj/Debug/MusicControl_Content.g.i.cs
+++ MusicControl/obj/Debug/MusicControl_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("visualresources/playbuttondisabled.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("visualresources/stopbuttonactive.png")]
 
 
