9b59534ee5b1a106b804bb095384bf7458e274c2
Julistian <urist9112@rambler.ru>
2019-09-10 20:54:47 +0300

    Изменил картинку фона и добавил логотип

--- MusicControl/obj/Debug/MusicControl_Content.g.i.cs
+++ MusicControl/obj/Debug/MusicControl_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("visualresources/editbuttondisabled.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("visualresources/awaitpicture.png")]
 
 
