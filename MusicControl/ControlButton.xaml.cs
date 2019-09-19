42ead357f0d496760231d13263ab3bb92029c31f
Julistian <urist9112@rambler.ru>
2019-09-19 18:20:20 +0300

    Добавлены новые кнопки

--- MusicControl/ControlButton.xaml.cs
+++ MusicControl/ControlButton.xaml.cs
@@ -1,18 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MusicControl
 {
@@ -25,10 +20,27 @@ namespace MusicControl
             IsButtonEnabled = true;
         }
 
-        public ImageBrush DisableBackgroundImage { get; set; }
-        public ImageBrush DisableBackgroundImageActive { get; set; }
-        public ImageBrush EnableBackgroundImage { get; set; }
-        public ImageBrush EnableBackgroundImageActive { get; set; }
+        //public ImageBrush DisableBackgroundImage { get; set; }
+
+        private ImageBrush _enableBackgroundImageActive;
+        private ImageBrush _disableBackgroundImage;
+        private ImageBrush _enableBackgroundImage;
+        public ImageBrush EnableBackgroundImage
+        {
+            get
+            {
+                return _enableBackgroundImage;
+            }
+            set
+            {
+                _enableBackgroundImage = value;
+                _disableBackgroundImage = GetGrayScaleImage(EnableBackgroundImage);
+                _enableBackgroundImageActive = GetbrighterImage(EnableBackgroundImage, 1.3f);
+                DoPropertyChanged("BackgroundImageActive");
+                DoPropertyChanged("BackgroundImage");
+            }
+        }
+        
 
         public ICommand ControlCommand
         {
@@ -46,19 +58,6 @@ namespace MusicControl
         public static DependencyProperty IsButtonEnabledProperty =
             DependencyProperty.Register("IsButtonEnabled", typeof(bool), typeof(ControlButton), new UIPropertyMetadata(false, Refresh));
 
-        //public bool IsButtonEnabled
-        //{
-        //    get { return (bool)GetValue(IsButtonEnabledProperty); }
-        //    set
-        //    {
-        //        SetValue(IsButtonEnabledProperty, value);
-        //        IsEnabled = value;
-        //        DoPropertyChanged("DisableButton");
-        //        DoPropertyChanged("BackgroundImageActive");
-        //        DoPropertyChanged("BackgroundImage");
-        //    }
-        //}
-
         private bool _isButtonEnabled;
 
         public bool IsButtonEnabled
@@ -74,17 +73,107 @@ namespace MusicControl
 
         public static void Refresh(DependencyObject property, DependencyPropertyChangedEventArgs args)
         {
-            ControlButton circularButton = (ControlButton)property;
-            //circularButton.IsButtonEnabled = (bool)args.NewValue;
-            circularButton.DoPropertyChanged("IsButtonEnabled");
-            circularButton.DoPropertyChanged("BackgroundImageActive");
-            circularButton.DoPropertyChanged("BackgroundImage");
-            circularButton.DoPropertyChanged("IsEnabled");
+            ControlButton controlButton = (ControlButton)property;
+            controlButton.DoPropertyChanged("IsButtonEnabled");
+            controlButton.DoPropertyChanged("BackgroundImageActive");
+            controlButton.DoPropertyChanged("BackgroundImage");
+            controlButton.DoPropertyChanged("IsEnabled");
         }
 
-        public ImageBrush BackgroundImageActive => !(bool)GetValue(IsButtonEnabledProperty) ? DisableBackgroundImageActive : EnableBackgroundImageActive;
+        private ImageBrush GetGrayScaleImage(ImageBrush image)
+        {
+            BitmapSource src = (BitmapSource)image.ImageSource;
+            Bitmap bmp = new Bitmap(
+            src.PixelWidth,
+            src.PixelHeight,
+            System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
+            BitmapData data = bmp.LockBits(
+              new Rectangle(System.Drawing.Point.Empty, bmp.Size),
+              ImageLockMode.WriteOnly,
+              System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
+            src.CopyPixels(
+              Int32Rect.Empty,
+              data.Scan0,
+              data.Height * data.Stride,
+              data.Stride);
+            bmp.UnlockBits(data);
+            Bitmap newBitmap = new Bitmap(bmp.Width, bmp.Height);
+            Graphics g = Graphics.FromImage(newBitmap);
+            ColorMatrix colorMatrix = new ColorMatrix(
+               new float[][]
+               {
+                    new float[] {.3f, .3f, .3f, 0, 0},
+                    new float[] {.59f, .59f, .59f, 0, 0},
+                    new float[] {.11f, .11f, .11f, 0, 0},
+                    new float[] {0, 0, 0, 1, 0},
+                    new float[] {0, 0, 0, 0, 1}
+               });
+            ImageAttributes attributes = new ImageAttributes();
+            attributes.SetColorMatrix(colorMatrix);
+            g.DrawImage(bmp, new Rectangle(0, 0, bmp.Width, bmp.Height),
+               0, 0, bmp.Width, bmp.Height, GraphicsUnit.Pixel, attributes);
+            g.Dispose();
 
-        public ImageBrush BackgroundImage => !(bool)GetValue(IsButtonEnabledProperty) ? DisableBackgroundImage : EnableBackgroundImage;
+            var bitmapSource = Imaging.CreateBitmapSourceFromHBitmap(newBitmap.GetHbitmap(),
+                              IntPtr.Zero,
+                              Int32Rect.Empty,
+                              BitmapSizeOptions.FromEmptyOptions());
+            var grayImage = new ImageBrush(bitmapSource);
+            return grayImage;
+        }
+
+        private ImageBrush GetbrighterImage(ImageBrush image, float brightness)
+        {
+            BitmapSource src = (BitmapSource)image.ImageSource;
+            Bitmap bmp = new Bitmap(
+            src.PixelWidth,
+            src.PixelHeight,
+            System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
+            BitmapData data = bmp.LockBits(
+              new Rectangle(System.Drawing.Point.Empty, bmp.Size),
+              ImageLockMode.WriteOnly,
+              System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
+            src.CopyPixels(
+              Int32Rect.Empty,
+              data.Scan0,
+              data.Height * data.Stride,
+              data.Stride);
+            bmp.UnlockBits(data);
+            Image img = (Image) bmp;
+            float b = brightness;
+            ColorMatrix cm = new ColorMatrix(new float[][]
+                {
+                    new float[] {b, 0, 0, 0, 0},
+                    new float[] {0, b, 0, 0, 0},
+                    new float[] {0, 0, b, 0, 0},
+                    new float[] {0, 0, 0, 1, 0},
+                    new float[] {0, 0, 0, 0, 1},
+                });
+            ImageAttributes attributes = new ImageAttributes();
+            attributes.SetColorMatrix(cm);
+            System.Drawing.Point[] points =
+            {
+                new System.Drawing.Point(0, 0),
+                new System.Drawing.Point(img.Width, 0),
+                new System.Drawing.Point(0, img.Height),
+            };
+            Rectangle rect = new Rectangle(0, 0, img.Width, img.Height);
+            Bitmap bm = new Bitmap(img.Width, img.Height);
+            using (Graphics gr = Graphics.FromImage(bm))
+            {
+                gr.DrawImage(img, points, rect,
+                    GraphicsUnit.Pixel, attributes);
+            }
+            var bitmapSource = Imaging.CreateBitmapSourceFromHBitmap(bm.GetHbitmap(),
+                                  IntPtr.Zero,
+                                  Int32Rect.Empty,
+                                  BitmapSizeOptions.FromEmptyOptions());
+            return new ImageBrush(bitmapSource); ;
+        }
+
+        public ImageBrush BackgroundImageActive => !(bool)GetValue(IsButtonEnabledProperty) ? _disableBackgroundImage : _enableBackgroundImageActive;
+
+        public ImageBrush BackgroundImage => !(bool)GetValue(IsButtonEnabledProperty) ? _disableBackgroundImage : _enableBackgroundImage;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
